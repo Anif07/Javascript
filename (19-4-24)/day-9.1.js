1b20e82236dee0484acfd24ebaec3cec6e897a69
Anif07 <mahammadanif22@gmail.com>
2024-04-19 12:42:48 +0530

    19-4-24

---
+++ (19-4-24)/day-9.1.js
@@ -0,0 +1,23 @@
+// var x = 123;
+// console.log(x);
+// console.log(x.toString());
+
+// console.log(x.toExponential(1));
+
+var x = 45.59;
+console.log(x.toFixed(0));
+console.log(x.toFixed(1));
+console.log(x.toFixed(2));
+console.log(x.toFixed(4));
+
+y = "10";
+console.log(Number(y));
+y = 25.364;
+console.log(parseInt(y));
+z = 20.253;
+console.log(parseFloat(z));
+
+x = 25.33;
+console.log(Number.isInteger(x));
+x = 25;
+console.log(Number.isInteger(x));
